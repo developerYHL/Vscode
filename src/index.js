60654e7d18837a25e02609c8f2848be497c8b9b1
developerYHL <developer.yhl@gmail.com>
2019-09-02 19:56:33 +0900

    post까지 완료

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const _ = require("lodash");
 const app = express();
-let users = [];
+let users = [{
+    id: 1,
+    name: "홍길동"
+},{
+    id: 2,
+    name: "강철수"
+}];
 let user = "null";
 
-app.use(express.json());    //  
+app.use(express.json()); 
 app.use(express.urlencoded({extended:true}));
 
 app.listen(3000);
@@ -17,24 +24,52 @@ app.listen(3000);
 // });
 
 app.get("/users", (req, res) => {
-  res.send("user get" + user.name);
+    let msg = "asd ";
+    res.send({msg , result : users});
+
+    if(users){
+        res.send("user get" + users.name);
+    }else{
+        res.send("error")     
+    }
+
+
 });
 
 app.get("/users/:id", (req,res)=> {
-    if(user.id == req.params){
-        
-        res.send("user " + user.name+ "get");
+    if(users && user.id == req.params.id){
+        res.send("user " + users.name+ "get");
+    }else{
+        res.send("error")     
     }
-    res.send("user "+ user.name + " get");
 });
 
 app.post("/users", (req,res)=> {
-    user = req.body;
-    res.send("user add"+ user.name + " post");
+    const check_user = _.find(users, ["id", req.params.id]);
+    let msg = req.body.id+" 아이디를 가진 유저가 이미 존재합니다.";
+    let success = false;
+    if(!check_user){
+        users.push(req.body);
+        msg = req.body.name+" 유저가 추가되었습니다.";
+        success = true;
+    }
+    res.send({msg, success})
 });
+
 app.put("/users/:id", (req,res)=> {
-    res.send("user "+ req.params.id + " put");
+    if(user && user.id == req.params.id){
+        res.send("user "+ req.params.id + " put");
+    }else{
+        res.send("error")             
+    }
 });
+
 app.delete("/users/:id", (req,res)=> {
-    res.send("user "+ req.params.id + " delete");
+    if(user && user.id == req.params.id){
+        user = null
+        res.send("user "+ req.params.id + " delete");
+    }
+    else{
+        res.send("user id" + req.params.id+"not exist")
+    }
 });
