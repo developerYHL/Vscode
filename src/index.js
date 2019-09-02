92b2e4bf5c263decf3cc5396a3f150e5251677b1
developerYHL <developer.yhl@gmail.com>
2019-09-02 19:10:07 +0900

    Practice 1 Start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const app = express();
+let users = [];
+let user = "null";
+
+app.use(express.json());    //  
+app.use(express.urlencoded({extended:true}));
 
 app.listen(3000);
 
-app.get("/", (rea, res) => {
-    res.send("hello world");
+// app.get("/", (req, res) => {
+//     let a= 10;
+//     let b = 20;
+//     let c = a+b;
+//     req.send(c);
+//     res.send("hello dsdsad")
+// });
+
+app.get("/users", (req, res) => {
+  res.send("user get" + user.name);
+});
+
+app.get("/users/:id", (req,res)=> {
+    if(user.id == req.params){
+        
+        res.send("user " + user.name+ "get");
+    }
+    res.send("user "+ user.name + " get");
+});
+
+app.post("/users", (req,res)=> {
+    user = req.body;
+    res.send("user add"+ user.name + " post");
+});
+app.put("/users/:id", (req,res)=> {
+    res.send("user "+ req.params.id + " put");
+});
+app.delete("/users/:id", (req,res)=> {
+    res.send("user "+ req.params.id + " delete");
 });
