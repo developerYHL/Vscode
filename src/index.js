528042c3a3b00d152c9922d8e04d0f8bea220ec9
developerYHL <developer.yhl@gmail.com>
2019-09-16 15:22:54 +0900

    practice 4, complete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const board_router = require("./route/board")
 app.use(express.json()); 
 app.use(express.urlencoded({extended:true}));
 app.use("/users", user_router);
-app.use("/", board_router)
+app.use("/board", board_router)
 
 app.listen(3000);
 
--- src/route/board.js
+++ src/route/board.js
@@ -1,24 +1,111 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("get all");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node_example", "root", "0000",{ hotst: "localhost", dialect: "mysql"});
+
+const check_sequlize_auth = async() =>{
+    try{
+        await sequelize.authenticate();
+        console.log("연결  성공");
+    }catch(err){
+        console.log("연결 실패", err);
+    }
+};
+check_sequlize_auth();
+
+const Board = sequelize.define("Board",{
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    contents:{
+        type : Sequelize.STRING,
+        allowNull: false
+    },
+    viewcount:{
+        type : Sequelize.INTEGER,
+        allowNull: false
+    }
 });
 
-router.get("/:id", (req, res) => {
-    res.send("get id");
+Board.sync({force:true}) .then(() => {
+    return Board.create({
+        title: "a",
+        contents: "a",
+        viewcount : 1
+    });
+}).then( () => {
+    return Board.create({
+        title: "b",
+        contents: "b",
+        viewcount : 2
+    });
 });
 
-router.post("/", (req, res) => {
-    res.send("get post");
+router.get("/", async(req,res)=> {
+    let result = await Board.findAll({
+        attributes: ["title"]
+    });
+    res.send(result);
 });
 
-router.put("/", (req, res) => {
-    res.send("get put");
+router.get("/title/:id", async(req,res)=> {
+    let result = await Board.findAll({
+        where:{
+            id: req.params.id
+        }
+    });
+    res.send(result);
 });
 
-router.delete("/", (req, res) => {
-    res.send("get delete");
+router.post("/", async(req, res)=> {
+    let result = false;
+    try{
+        await Board.create({ title: req.body.title, contents: req.body.contents, viewcount: req.body.viewcount });
+        result = true;
+    }catch(err){
+        console.error(err);
+    }
+    res.send(result);
+
+});
+
+router.put("/:title", async(req, res) => {
+    let result = false;
+    try {
+        await Board.update(
+            {
+                title: req.body.title,
+                contents: req.body.contents,
+                viewcount: req.body.viewcount
+            }, {
+                where: {
+                    title: req.params.title
+                }
+            }
+        );
+        result = true;
+    } catch (err) {
+        console.error(err);
+    }
+
+    res.send(result);
+});
+
+router.delete("/:title", async(req, res) => {
+    let result = false;
+    try {
+        await Board.destroy({
+            where: {
+                title: req.params.title
+            }
+        });
+        result = true;
+    } catch (err) {
+        console.error(err);
+    }
+    res.send(result);
 });
 
 module.exports = router;
